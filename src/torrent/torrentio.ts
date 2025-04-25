038424f7c344c93a73f3ca75514dced151882f72
tareksrour <tarek@sirenanalytics.com>
2025-04-25 16:17:11 +0300

    Update Torrentio API URL and modify parsing logic

--- src/torrent/torrentio.ts
+++ src/torrent/torrentio.ts
@@ -9,16 +9,16 @@ export const searchTorrentio = async (
     id: string,
     type: string
   ): Promise<TorrentSearchResult[]> => {
-    const url = `https://torrentio.strem.fun/stream/${type}/${id}.json`;
+    const url = `https://torrentio.elfhosted.com/stream/${type}/${id}.json`;
     const resp= await axios.get(url);
     const data = resp.data;
     return data?.streams?.map((torrent: any) => {
-        const match = torrent.title.match(/👤 (\d+) 💾 ([\d.]+) (KB|MB|GB)/);
-        const users = parseInt(match[1], 10);
-        const size = parseFloat(match[2]);
-        const unit = match[3];
+        const match = torrent.title.match(/💾 ([\d.]+) (KB|MB|GB)/);
+        const users = 1 //parseInt(match[1], 10);
+        const size = parseFloat(match[1]);
+        const unit = match[2];
         return {
-            name: torrent.title.split("👤")[0].trim(),
+            name: torrent.title.split("💾")[0].trim(),
             tracker: "Torrentio",
             seeds: users,
             size: size * unitMultipliers[unit],
